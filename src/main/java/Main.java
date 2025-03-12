248b0b801ee2d04f0051d6cec6e9e2d24d2fd402
chanwon <clw8679@naver.com>
2025-03-12 16:26:01 +0900

    설명 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,13 @@ import service.MemberService;
 
 public class Main {
     public static void main(String[] args) {
+        /* spring 사용전
+            MemberRepository memberRepository = new MemoryRepository(); -> MemoryRepository 다른 MemberRepository로 변경시 코드 변경 필요
+            MemberService memberService = new MemberServiceImpl(memberRepository); -> 사용자가 직접 memberRepository를 생성하고 memberService에 주입
+        */
+
         ApplicationContext context = new AnnotationConfigApplicationContext(AnnotationConfig.class);
+        // memberService를 사용할 때 주입해야 하는 객체를 알 필요가 없음
         MemberService memberService = (MemberService) context.getBean("memberService");
 
         Member memberA = memberService.findById(1L);
